65ea4d0b5e8892c01c5d9a0b427c403ba473231b
HundredPercentCoverage <bunglenutter@gmail.com>
2020-03-15 00:20:12 +0000

    New main audio player based on hook

---
+++ src/components/MainPlayer.jsx
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect } from 'react';
+import { Button, Icon } from 'semantic-ui-react';
+import { PlayersContext } from '../App';
+import useAudio from '../hooks/useAudio';
+
+function MainPlayer({ selectedFile }) {
+  const { playing, toggle, ended } = useAudio(selectedFile);
+  const { dispatch } = useContext(PlayersContext);
+
+  useEffect(() => {
+    if (ended) {
+      dispatch({ type: 'selectnext' });
+    }
+  }, [ended, dispatch]);
+
+  return (
+    <>
+      <Button.Group>
+        <Button icon onClick={() => dispatch({ type: 'selectprevious' })}>
+          <Icon name='fast backward' />
+        </Button>
+        <Button icon onClick={toggle}>
+          <Icon name={ playing ? 'pause' : 'play' } color={playing ? 'blue' : 'green'} />
+        </Button>
+        <Button icon onClick={() => dispatch({ type: 'selectnext' })}>
+          <Icon name='fast forward' />
+        </Button>
+      </Button.Group>
+      <span style={{ padding: '1em' }}>{selectedFile ? selectedFile.name : 'No file'}</span>
+    </>
+  )
+}
+
+export default MainPlayer
